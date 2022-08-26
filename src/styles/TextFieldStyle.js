ca994555af1fb83e9d10fcdfec9af8306589cd9b
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2022-08-26 18:25:19 +0700

    feat: custom style text field mui

---
+++ src/styles/TextFieldStyle.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const textFieldStyles = makeStyles({
+	field: {
+		color: '#71b9be',
+		'&:hover': {
+			color: '#71b9be',
+		},
+	},
+});
