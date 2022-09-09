9432ce57c1fcaba7102e10e8d04e1778ef95e960
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2022-09-09 23:30:45 +0700

    feat: add about us card ui

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,13 +1,18 @@
+import {Card, CardContent } from '@mui/material';
 import React from 'react';
 
 const About = () => {
 	return (
-		<div>
-			<span className='pageTitle'>About Us</span>
-			<p>
+		<Card style={{borderRadius: 8.0}}>
+			<CardContent>
+				<div>
+					<span className='pageTitle'>About Us</span>
+					<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</p>
-		</div>
+					</p>
+				</div>
+			</CardContent>
+		</Card>
 	);
 };
 
