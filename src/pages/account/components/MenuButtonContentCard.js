f6277273370018bca2697c58befa94891698df34
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2022-09-10 12:14:54 +0700

    feat: seperate card component to be independent for profile and menu button content

---
+++ src/pages/account/components/MenuButtonContentCard.js
@@ -0,0 +1,56 @@
+/* eslint-disable react/prop-types */
+import { ArrowForwardIosRounded } from '@mui/icons-material';
+import { Card, CardContent, List, ListItemButton, ListItemText, Typography } from '@mui/material';
+import React from 'react';
+import { DefaultButton } from '../../../components/Button/Button';
+
+export const MenuButtonContentCard = ({directToFavoritePage, directToMyRecipePage, directToAboutPage, logout}) => {
+	return (
+		<Card 
+			className='glassmorphism_card' 
+			style={{
+				background: 'rgba(113, 185, 190, 0.39)',
+				borderRadius: '15px',
+				boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+				backdropFilter: 'blur(6.4px)',
+				border: '1px solid rgba(255, 255, 255, 0.21)'
+			}}>
+			<CardContent>
+				<List
+					dense
+				>
+					<ListItemButton
+						divider={true}
+						onClick={directToFavoritePage}>
+						<ListItemText
+							disableTypography
+							primary={<Typography>Favorites</Typography>}
+						/>
+						<ArrowForwardIosRounded/>
+					</ListItemButton>
+					<ListItemButton 
+						divider={true}
+						onClick={directToMyRecipePage}>
+						<ListItemText 
+							disableTypography
+							primary={<Typography>My Recipes</Typography>}
+						/>
+						<ArrowForwardIosRounded/>
+					</ListItemButton>
+					<ListItemButton 
+						divider={true}
+						onClick={directToAboutPage}>
+						<ListItemText 
+							disableTypography
+							primary={<Typography>About</Typography>}
+						/>
+						<ArrowForwardIosRounded/>
+					</ListItemButton>
+				</List>
+				<div style={{paddingLeft: 17}}>
+					<DefaultButton title='Logout' handlingEvents={logout}/>
+				</div>
+			</CardContent>
+		</Card>
+	);
+};
