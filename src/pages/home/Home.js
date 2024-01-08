d8ccfc3a457d685e1d6c693732cdee0ec7cf25e7
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2024-01-08 15:55:03 +0700

    chore(home): comment the covid information

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -9,9 +9,9 @@ import { ErrorSnackbar } from '../../components/Snackbar/CustomizedSnackbars';
 import { checkExpiredToken } from '../../utils/checkExpiredToken';
 import { useUserDispatch } from '../../context/UserContext';
 import { useHistory } from 'react-router-dom';
-import { Covid19InfoSection } from './components/Covid19InfoSection';
-import { getIndonesianCovid19Data } from './service/service_rest_api';
-import { convertNumberToDecimal } from '../../utils/convertNumberToDecimal';
+// import { Covid19InfoSection } from './components/Covid19InfoSection';
+// import { getIndonesianCovid19Data } from './service/service_rest_api';
+// import { convertNumberToDecimal } from '../../utils/convertNumberToDecimal';
 
 const Home = () => {
 	let token;
@@ -21,7 +21,7 @@ const Home = () => {
 	const [totalPage, setTotalPage] = useState(0);
 	const [content, setContent] = useState([]);
 	const [message, setMessage] = useState('');
-	const [covid19Data, setCovid19Data] = useState({});
+	// const [covid19Data, setCovid19Data] = useState({});
 	const {loading, error, data} = useQuery(RECIPE_LIST,{
 		variables: {
 			page
@@ -49,7 +49,7 @@ const Home = () => {
 	};
 
 	useEffect(async () => {
-		setCovid19Data( await getIndonesianCovid19Data());
+		// setCovid19Data( await getIndonesianCovid19Data());
 		if (data) {
 			setContent(data.recipeList);	
 			setTotalPage(data.recipeList.meta.totalPage);
@@ -63,10 +63,10 @@ const Home = () => {
 		return () => clearTimeout(timer);
 	},[page, data, error]);
 	
-	const covid19NewCases = convertNumberToDecimal(covid19Data.NewCases);
-	const covid19TotalConfirmed = convertNumberToDecimal(covid19Data.TotalCases);
-	const covid19TotalDeaths = convertNumberToDecimal(covid19Data.TotalDeaths);
-	const covid19TotalRecovered = convertNumberToDecimal(covid19Data.TotalRecovered);
+	// const covid19NewCases = convertNumberToDecimal(covid19Data.NewCases);
+	// const covid19TotalConfirmed = convertNumberToDecimal(covid19Data.TotalCases);
+	// const covid19TotalDeaths = convertNumberToDecimal(covid19Data.TotalDeaths);
+	// const covid19TotalRecovered = convertNumberToDecimal(covid19Data.TotalRecovered);
 
 	return (
 		<>
@@ -92,12 +92,12 @@ const Home = () => {
 			</Loading>
 				: error ? <ErrorSnackbar message={message}/>
 					: <div>
-						<Covid19InfoSection
+						{/* <Covid19InfoSection
 							totalConfirmed={covid19TotalConfirmed}
 							totalRecovered={covid19TotalRecovered}
 							totalDeaths={covid19TotalDeaths}
 							newCases={covid19NewCases}
-						/>
+						/> */}
 						<div className='home'>
 							{
 								content.recipes && content.recipes.map((c) => <SingleContent 
